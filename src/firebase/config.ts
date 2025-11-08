cc15237b64b90956e64f04b5d332bf448477095c
Reel Lookz <reellookz@gmail.com>
2025-11-08 16:56:37 +0000

    no i just need to use       the buckect thats all

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyCwmeORwHLnQ8ESauE9cobUYE-stkiq7fM",
-  authDomain: "studio-3439019714-ce2b4.firebaseapp.com",
-  projectId: "studio-3439019714-ce2b4",
+  authDomain: "studio-9439717090-1fb6e.firebaseapp.com",
+  projectId: "studio-9439717090-1fb6e",
   storageBucket: "studio-3439019714-ce2b4.firebasestorage.app",
   messagingSenderId: "779726117709",
   appId: "1:779726117709:web:11b8676cd7b5fb2cec3504"
