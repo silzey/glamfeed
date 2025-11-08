a72515b3d0b7bf9092c68bdcf4dc081725fb9b95
Reel Lookz <reellookz@gmail.com>
2025-11-08 17:34:10 +0000

    replace config with // Import the functions you need from the SDKs you n

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,18 @@
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 export const firebaseConfig = {
   apiKey: "AIzaSyCwmeORwHLnQ8ESauE9cobUYE-stkiq7fM",
   authDomain: "studio-9439717090-1fb6e.firebaseapp.com",
   projectId: "studio-9439717090-1fb6e",
-  storageBucket: "studio-3439019714-ce2b4.firebasestorage.app",
+  storageBucket: "studio-9439717090-1fb6e.firebasestorage.app",
   messagingSenderId: "779726117709",
   appId: "1:779726117709:web:11b8676cd7b5fb2cec3504"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
