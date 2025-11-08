71117b3127ce1e220dff4b3896551882b1536046
Reel Lookz <reellookz@gmail.com>
2025-11-08 12:08:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9439717090-1fb6e",
+  "appId": "1:779726117709:web:11b8676cd7b5fb2cec3504",
+  "apiKey": "AIzaSyCwmeORwHLnQ8ESauE9cobUYE-stkiq7fM",
+  "authDomain": "studio-9439717090-1fb6e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "779726117709"
+};
