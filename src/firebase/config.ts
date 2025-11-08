b9cc262a2f261e4b16e5253e3576ed593cfc5abe
Reel Lookz <reellookz@gmail.com>
2025-11-08 16:51:42 +0000

    addd this // Import the functions you need from the SDKs you need import

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCwmeORwHLnQ8ESauE9cobUYE-stkiq7fM",
+  apiKey: "AIzaSyD4CU0IU8fZQyNvHqIodpU8dNJfn-9L2T4",
   authDomain: "studio-3439019714-ce2b4.firebaseapp.com",
+  databaseURL: "https://studio-3439019714-ce2b4-default-rtdb.firebaseio.com",
   projectId: "studio-3439019714-ce2b4",
-  storageBucket: "studio-3439019714-ce2b4.appspot.com",
-  messagingSenderId: "779726117709",
-  appId: "1:779726117709:web:11b8676cd7b5fb2cec3504"
+  storageBucket: "studio-3439019714-ce2b4.firebasestorage.app",
+  messagingSenderId: "972854038727",
+  appId: "1:972854038727:web:d34d19ecf09a58f2d26ee2"
 };
