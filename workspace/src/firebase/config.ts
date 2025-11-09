ab4786dab138b23a5bf0b954cdfd14ea7549c2a9
Reel Lookz <reellookz@gmail.com>
2025-11-09 20:04:18 +0000

    add these config // Import the functions you need from the SDKs you need

---
+++ workspace/src/firebase/config.ts
@@ -0,0 +1,17 @@
+
+'use client';
+
+// It is recommended to initialize Firebase without a config object.
+// Firebase App Hosting provides the configuration automatically.
+// See: https://firebase.google.com/docs/hosting/frameworks/nextjs?hl=en&authuser=0#initialize-firebase-in-your-app
+// The config object is included below as a reference.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCwmeORwHLnQ8ESauE9cobUYE-stkiq7fM",
+  authDomain: "studio-9439717090-1fb6e.firebaseapp.com",
+  projectId: "studio-9439717090-1fb6e",
+  storageBucket: "studio-9439717090-1fb6e.appspot.com",
+  messagingSenderId: "779726117709",
+  appId: "1:779726117709:web:11b8676cd7b5fb2cec3504",
+  measurementId: "G-FBRQCW4X60"
+};
