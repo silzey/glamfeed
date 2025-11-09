86a53fd0d8ab6d1e71c0c7df05a6ea6912a0e9f7
Reel Lookz <reellookz@gmail.com>
2025-11-09 11:20:29 +0000

    the loader is stuck make it wider slower and leaves when the page loads

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -100,7 +100,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'spin-fast': 'spin-fast 0.5s linear infinite',
+        'spin-fast': 'spin-fast 1s linear infinite',
       },
     },
   },
